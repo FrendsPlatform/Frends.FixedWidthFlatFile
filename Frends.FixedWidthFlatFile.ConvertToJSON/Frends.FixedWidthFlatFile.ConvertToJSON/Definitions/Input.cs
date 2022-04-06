20abc6b6c179ad74236ae86cee72af444c6d5260
L3O\ahlstve <vertti.ahlsten@hiq.fi>
2022-04-06 14:26:01 +0300

    Unnecessary files cleaned.

--- Frends.FixedWidthFlatFile.ConvertToJSON/Frends.FixedWidthFlatFile.ConvertToJSON/Definitions/Input.cs
+++ Frends.FixedWidthFlatFile.ConvertToJSON/Frends.FixedWidthFlatFile.ConvertToJSON/Definitions/Input.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static Frends.FixedWidthFlatFile.ConvertToJSON.Definitions.Enums;
 
 namespace Frends.FixedWidthFlatFile.ConvertToJSON.Definitions
 {
