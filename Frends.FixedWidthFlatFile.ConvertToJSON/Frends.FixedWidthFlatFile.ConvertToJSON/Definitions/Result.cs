fb0fb93acc268224e547fb240d7e883ea1608421
helvertti <vertti.ahlsten@hiq.fi>
2022-04-07 11:01:25 +0300

    Unnecessary displayformat deleted.

--- Frends.FixedWidthFlatFile.ConvertToJSON/Frends.FixedWidthFlatFile.ConvertToJSON/Definitions/Result.cs
+++ Frends.FixedWidthFlatFile.ConvertToJSON/Frends.FixedWidthFlatFile.ConvertToJSON/Definitions/Result.cs
@@ -7,7 +7,6 @@ namespace Frends.FixedWidthFlatFile.ConvertToJSON.Definitions
         /// <summary>
         /// Column name. If input data contains Header row and value is left empty, header value is used as name.
         /// </summary>
-        [DisplayFormat(DataFormatString = "Text")]
         public string Data { get; private set; }
 
         /// <summary>
