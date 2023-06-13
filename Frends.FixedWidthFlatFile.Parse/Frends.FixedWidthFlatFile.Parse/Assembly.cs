414cd17aa6d65e6ef20884f948ec7d00dc4627f4
ttossavainen <teemu.tossavainen@hiq.fi>
2023-06-13 07:12:05 +0300

    Doc updates

--- Frends.FixedWidthFlatFile.Parse/Frends.FixedWidthFlatFile.Parse/Assembly.cs
+++ Frends.FixedWidthFlatFile.Parse/Frends.FixedWidthFlatFile.Parse/Assembly.cs
@@ -1,3 +1,3 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly:InternalsVisibleTo("Frends.FixedWidthFlatFile.Parse.Tests")]
+[assembly: InternalsVisibleTo("Frends.FixedWidthFlatFile.Parse.Tests")]
